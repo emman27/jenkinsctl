9cf227d703743d8e3c9cc15b6fae4fda77520e77
Emmanuel Goh <emmanuel@visenze.com>
2018-11-26 23:55:05 +0800

    Shorten get code

--- pkg/api/parameters/get.go
+++ pkg/api/parameters/get.go
@@ -5,21 +5,16 @@ import (
 
 	"github.com/emman27/jenkinsctl/pkg/api"
 	"github.com/emman27/jenkinsctl/pkg/api/builds"
-	"github.com/golang/glog"
 )
 
 const parameterActionClass = "hudson.model.ParametersAction"
 
 // Get the parameters for a particular job and build
 func Get(c *api.JenkinsClient, jobName string, buildID int) (*Parameters, error) {
-	var (
-		build *builds.Build
-		err   error
-	)
-	if build, err = builds.Get(c, jobName, buildID); err != nil {
+	build, err := builds.Get(c, jobName, buildID)
+	if err != nil {
 		return nil, err
 	}
-	glog.Infof("Build Actions found: %v", build.Actions)
 	return getParams(build)
 }
 

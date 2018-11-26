ed627c8dd8f5333fa87af73ebb908c7172a666c2
Emmanuel Goh <emmanuel@visenze.com>
2018-11-26 10:55:04 +0800

    Add a test for builds to json

--- pkg/api/builds/types_test.go
+++ pkg/api/builds/types_test.go
@@ -13,3 +13,18 @@ func TestBuilds_Headers_Rows_Length(t *testing.T) {
 	headers := new(Builds).Headers()
 	assert.Equal(t, len(headers), len(builds.Rows()[0]), "Headers and Rows have different lengths")
 }
+
+func TestBuilds_JSON(t *testing.T) {
+	builds := Builds{
+		{
+			Class:       "Something",
+			Actions:     []BuildAction{},
+			ID:          24,
+			Result:      Success,
+			Description: "Just a job",
+			Timestamp:   int64(123456789),
+			Duration:    int64(30),
+		},
+	}
+	assert.Equal(t, "[{\"_class\":\"Something\",\"actions\":[],\"number\":24,\"result\":\"SUCCESS\",\"description\":\"Just a job\",\"timestamp\":123456789,\"duration\":30}]", string(builds.JSON()))
+}

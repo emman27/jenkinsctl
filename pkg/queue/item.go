0c8c53c95504bc72dfba8991bdc21ffc6cf0e546
Emmanuel Goh <emmanuel@visenze.com>
2018-12-04 11:00:23 +0800

    Add the task as part of the structure

--- pkg/queue/item.go
+++ pkg/queue/item.go
@@ -7,12 +7,19 @@ type Item struct {
 	ID         int         `json:"id"`
 	URL        string      `json:"url"`
 	Executable *Executable `json:"executable"`
+	Task       Task        `json:"task"`
 }
 
 // Executable is an execution of the queue item
 type Executable struct {
 	Number int    `json:"number"`
 	URL    string `json:"url"`
+}
+
+// Task is the scheduled task for the queue item
+type Task struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 // Executing returns a boolean indicating whether a queue Item is currently executing
